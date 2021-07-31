496e05d623993484cebe6207e25a5640c4c6e384
dante-signal31 <dante.signal31@gmail.com>
2021-07-31 10:33:56 +0200

    Tests for ops module ready.

---
+++ TestCommon/fs/ops.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace TestCommon.fs
+{
+    public class ops
+    {
+        /// <summary>
+        /// Delete an specific file.
+        /// </summary>
+        /// <param name="filePath"> String with the absolute path to file.</param>
+        public static void delete_file(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Delete all files set in given list.
+        /// </summary>
+        /// <param name="files">List with absolute pathname for files to remove.</param>
+        /// <param name="ignoreMissing">If true does not return an error if any of files actually does not exists.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void delete_files(string[] files, bool ignoreMissing)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Copy an specific file.
+        /// </summary>
+        /// <param name="sourceFilePath">String with absolute pathname to original file.</param>
+        /// <param name="destinationFilePath">String with absolute pathname to copied file.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void copy_file(string sourceFilePath, string destinationFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Copy all files in a given list to a given destination folder.
+        ///
+        /// Original file names are kept untouched.
+        /// </summary>
+        /// <param name="files">List with absolute pathnames as strings.</param>
+        /// <param name="destinationFolderPath">Absolute path name to folder where to copy files into.</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public static void copy_files(string[] files, string destinationFolderPath)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ TestCommonTests/OpsTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using NUnit.Framework;
+using TestCommon.fs;
+
+namespace TestCommonTests
+{
+    public class OpsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDeleteFile()
+        {
+            string tempFilePath = Path.GetTempFileName();
+            Assert.True(File.Exists(tempFilePath));
+            ops.delete_file(tempFilePath);
+            Assert.False(File.Exists(tempFilePath));
+        }
+
+        [Test]
+        public void TestDeleteExistingFiles()
+        {
+            string[] tempFilePathnames = new string[]
+            {
+                Path.GetTempFileName(),
+                Path.GetTempFileName(),
+                Path.GetTempFileName()
+            };
+            foreach (string filePathName in tempFilePathnames)
+            {
+                Assert.True(File.Exists(filePathName));
+            }
+            ops.delete_files(tempFilePathnames, false);
+            foreach (string filePathName in tempFilePathnames)
+            {
+                Assert.False(File.Exists(filePathName));
+            }
+        }
+
+        [Test]
+        public void TestDeletingNonExistingFiles()
+        {
+            string tempFilePathName = Path.GetTempFileName();
+            Assert.True(File.Exists(tempFilePathName));
+            string unexistingFile = Path.Combine(Path.GetTempPath(), "123456789.nex");
+            string[] filesToDelete = new string[]
+            {
+                tempFilePathName,
+                unexistingFile
+            };
+            // Check non-existing files are detected when ignore_missing is False.
+            Assert.Throws<FileNotFoundException>(() => ops.delete_files(filesToDelete, false));
+            //Check non-existing files don't raise any exception when ignore_missing is true.
+            tempFilePathName = Path.GetTempFileName();
+            Assert.True(File.Exists(tempFilePathName));
+            filesToDelete = new string[]
+            {
+                tempFilePathName,
+                unexistingFile
+            };
+            ops.delete_files(filesToDelete,true);
+            Assert.False(File.Exists(tempFilePathName));
+        }
+
+        [Test]
+        public void TestCopyFile()
+        {
+            string tempFilePathName = Path.GetTempFileName();
+            string tempFileName = Path.GetFileName(tempFilePathName);
+            using (Temp tempDir = new Temp(Temp.TempType.Folder))
+            {
+                string expectedFilePathName = Path.Combine(tempDir.TempPath, tempFileName);
+                Assert.False(File.Exists(expectedFilePathName));
+                ops.copy_file(tempFilePathName, expectedFilePathName);
+                Assert.True(File.Exists(expectedFilePathName));
+            }
+        }
+
+        [Test]
+        public void TestCopyFiles()
+        {
+            // Create temporal files at default temp folder.
+            var tempFileNames = from _ in Enumerable.Range(0, 2) select Path.GetTempPath();
+            // Copy temporal files to our own temporal folder.
+            using (Temp tempDir = new Temp(Temp.TempType.Folder))
+            {
+                ops.copy_files(tempFileNames.ToArray(), tempDir.TempPath);
+                // Check all created and copied files are actually at our temporal folder.
+                Assert.True(tempFileNames.All(
+                    file => File.Exists(
+                        Path.Combine(tempDir.TempPath,
+                            Path.GetFileName(file))
+                        )
+                    ));
+            }
+        }
+    }
+}
