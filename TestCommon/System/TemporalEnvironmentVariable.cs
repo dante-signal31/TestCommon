b33fd44d35ab68233bb211b8660517aa4cc4d26a
dante-signal31 <dante.signal31@gmail.com>
2021-08-01 18:21:42 +0200

    TemporalEnvironmentVariable now works.

---
+++ TestCommon/System/TemporalEnvironmentVariable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TestCommon.System
+{
+    /// <summary>
+    /// Context manager to create an environment variable to perform test with it.
+    /// 
+    /// If environment variable already existed then former value is stored before
+    /// setting the new one. Former value is restored when context manager is left.
+    /// This context manager returns an instance that let you update env var value using
+    /// its set_var method.
+    ///
+    /// Be aware that depending of variable context you may need administrative access
+    /// to set it. 
+    /// </summary>
+    public class TemporalEnvironmentVariable: IDisposable
+    {
+        private string _oldValue;
+        private string _name;
+        private string _value;
+        private EnvironmentVariableTarget _context;
+
+        public string Name => _name;
+        public string Value => _value;
+        
+        /// <summary>
+        /// Constructor for TemporalEnvironmentVariable context manager.
+        /// </summary>
+        /// <param name="name">Name of environment variable.</param>
+        /// <param name="value">Value that environment variable should have.</param>
+        /// <param name="context">Optionally you can set context where to look for this variable.
+        /// Possible values are: EnvironmentVariableTarget.Process, EnvironmentVariableTarget.User and
+        /// EnvironmentVariableTarget.Machine. Default is EnvironmentVariableTarget.Process.
+        /// </param>
+        public TemporalEnvironmentVariable(string name, string value, EnvironmentVariableTarget context=EnvironmentVariableTarget.Process)
+        {
+            _oldValue = Environment.GetEnvironmentVariable(name, context);
+            _name = name;
+            setVar(value);
+            _context = context;
+        }
+
+        public void setVar(string newValue)
+        {
+            Environment.SetEnvironmentVariable(_name, newValue, _context);
+            _value = newValue;
+        }
+        
+        public void Dispose()
+        {
+            if (_oldValue != null) Environment.SetEnvironmentVariable(_name, _oldValue, _context);
+        }
+    }
+}
---
+++ TestCommonTests/EnvTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using TestCommon.Random;
+using TestCommon.System;
+
+namespace TestCommonTests
+{
+    public class EnvTests
+    {
+        /// <summary>
+        /// Get a random name for an environment variable after checking it does not exist yet.
+        /// </summary>
+        /// <param name="context">Optionally you can set context where to look for this variable.
+        /// Possible values are: EnvironmentVariableTarget.Process, EnvironmentVariableTarget.User and
+        /// EnvironmentVariableTarget.Machine. Default is EnvironmentVariableTarget.Machine.</param>
+        /// <returns></returns>
+        private static string GetNotExistingEnvVarName(EnvironmentVariableTarget context = EnvironmentVariableTarget.Process)
+        {
+            const int desiredLength = 10;
+            string randomName = "";
+            string recoveredValue = "";
+            do {
+                randomName = Strings.RandomString(desiredLength);
+                recoveredValue = Environment.GetEnvironmentVariable(randomName, context);
+            } while (recoveredValue != null);
+            return randomName;
+        }
+
+        [Test]
+        public void TestNotPreviouslyExistingEnvironmentVariableCreation()
+        {
+            const string DesiredValue = "Hello";
+            string newVarName = GetNotExistingEnvVarName();
+            Assert.True(Environment.GetEnvironmentVariable(newVarName) == null);
+            using (TemporalEnvironmentVariable temporalEnvironmentVariable = new TemporalEnvironmentVariable(newVarName, DesiredValue))
+            {
+                string recoveredValue = Environment.GetEnvironmentVariable(newVarName);
+                if (recoveredValue == null)
+                {
+                    Assert.Fail();
+                }
+                else
+                {
+                    Assert.True(recoveredValue.Equals(DesiredValue));
+                }
+            }
+        }
+
+        [Test]
+        public void TestPreviouslyExistingEnvironmentVariableCreation()
+        {
+            const string oldValue = "Bye";
+            const string desiredValue = "Hello";
+            // Create a previous env var.
+            string newVarName = GetNotExistingEnvVarName();
+            Environment.SetEnvironmentVariable(newVarName, oldValue);
+            // Check we can store a new value.
+            using (TemporalEnvironmentVariable temporalEnvironmentVariable = new TemporalEnvironmentVariable(newVarName, desiredValue))
+            {
+                Assert.True(desiredValue.Equals(
+                    Environment.GetEnvironmentVariable(newVarName)));
+            }
+            // Check old value has been restored.
+            Assert.True(oldValue.Equals(Environment.GetEnvironmentVariable(newVarName)));
+        }
+
+        [Test]
+        public void TestSetVar()
+        {
+            const string desiredValue = "Hello";
+            const string newValue = "Hello World";
+            // Give a previous value to env var.
+            string newVarName = GetNotExistingEnvVarName();
+            using (TemporalEnvironmentVariable tempTemporalEnvironmentVariableVar = new TemporalEnvironmentVariable(newVarName, desiredValue))
+            {
+                Assert.True(desiredValue.Equals(
+                    Environment.GetEnvironmentVariable(newVarName)));
+                // Update temporalEnvironmentVariable var value.
+                tempTemporalEnvironmentVariableVar.setVar(newValue);
+                Assert.True(newValue.Equals(
+                    Environment.GetEnvironmentVariable(newVarName)));
+            }
+        }
+    }
+}
