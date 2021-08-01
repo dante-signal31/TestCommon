5c7ae4280c59b086e3a81d0bfb7f755895af4dac
dante-signal31 <dante.signal31@gmail.com>
2021-08-01 08:47:16 +0200

    Random string generator ready.

---
+++ TestCommon/random/strings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace TestCommon.random
+{
+    public class strings
+    {
+        public static string RandomString(int length)
+        {
+            string alphanumericAlphabet = "qwertyuiopasdfghjklzxcvbnm1234567890";
+            int alphanumericAlphabetLength = alphanumericAlphabet.Length;
+            Random rnd = new Random();
+            string generatedString = "";
+            foreach (int i in Enumerable.Range(0, length))
+            {
+                char selectedChar = alphanumericAlphabet[rnd.Next(alphanumericAlphabetLength)];
+                generatedString= String.Concat(generatedString, selectedChar);
+            }
+            return generatedString;
+        }
+    }
+}
---
+++ TestCommonTests/RandomTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestCommon.random;
+
+namespace TestCommonTests
+{
+    public class RandomTests
+    {
+        [Test]
+        public void TestRandomStringLength()
+        {
+            const int desiredLength = 7;
+            string generatedString = strings.RandomString(desiredLength);
+            int generatedLength = generatedString.Length;
+            Assert.AreEqual(desiredLength, generatedLength);
+        }
+    }
+}
