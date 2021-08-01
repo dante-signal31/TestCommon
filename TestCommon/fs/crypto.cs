20f0e36376175d9743e5a18cea5282df54ee558e
dante-signal31 <dante.signal31@gmail.com>
2021-08-01 07:47:54 +0200

    crypto module now works.

---
+++ TestCommon/fs/crypto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace TestCommon.fs
+{
+    /// <summary>
+    ///  Cryptographic functions for your tests. Here you can find hashing functions to check file contents.
+    /// </summary>
+    public class crypto
+    {
+        /// <summary>
+        /// Hash file content with SHA-256.
+        ///
+        ///  This way we can check two files have same content.
+        /// </summary>
+        /// <param name="filePath">Absolute path name.</param>
+        /// <returns>File hash string.</returns>
+        public static string hash_file(string filePath)
+        {
+            byte[] content = File.ReadAllBytes(filePath);
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashValue = sha256.ComputeHash(content);
+                string hashString = BitConverter.ToString(hashValue).Replace("-", "").ToLower();
+                return hashString;
+            }
+        }
+    }
+}
---
+++ TestCommonTests/TestCrypto.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+using NUnit.Framework;
+using TestCommon.fs;
+
+namespace TestCommonTests
+{
+    public class TestCrypto
+    {
+        [Test]
+        public void TestHashFile()
+        {
+            string EXPECTED_HASH = "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2";
+            using (Temp tempFile = new Temp(Temp.TempType.File))
+            {
+                byte[] contentBytes = Encoding.ASCII.GetBytes("foobar");
+                FileStream writeStream = File.OpenWrite(tempFile.TempPath);
+                writeStream.Write(contentBytes);
+                writeStream.Flush();
+                writeStream.Close();
+                string recoveredFileHash = crypto.hash_file(tempFile.TempPath);
+                Assert.True(EXPECTED_HASH.Equals(recoveredFileHash));
+            }
+        }
+    }
+}
